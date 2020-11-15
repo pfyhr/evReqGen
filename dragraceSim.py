8101afcfa99f65cf9870075d65b92a6a0e8d2fe2
Pontus <pontus@fyhr.nu>
2020-11-15 18:01:41 +0100

    working Python port of the old matlab code from https://www.fyhr.nu/fyhrsq/dragraceSim/

---
+++ dragraceSim.py
@@ -0,0 +1,132 @@
+
+import sympy as sym
+from sympy.solvers.solveset import linsolve
+import numpy as np
+
+def topGradeSpeed(Cd, Cr, g, rhoAir, frontArea, mass, grade, velocity, wheelRadius):
+    Froll = Cr * mass * g
+    Fdrag = Cd*frontArea*rhoAir*(velocity)**2/2
+    Fincl = mass * g * grade
+    Ftrac = Froll + Fdrag + Fincl
+    wheelTorque = Ftrac*wheelRadius
+
+    return wheelTorque
+
+def tractionMax(mass, g, wtRearFrac, wheelbase, cgh, driveWheel, muTire):
+    Fr = sym.Symbol('Fr')
+    Ff = sym.Symbol('Ff')
+    Fa = sym.Symbol('Fa')
+
+    if driveWheel == 'AWD':
+        wheelForceMax = mass*g*muTire
+    elif driveWheel == 'FWD':
+        eq1 = Fr + Ff - mass*g
+        eq2 = Fr*(1-wtRearFrac)*wheelbase - Ff*wtRearFrac*wheelbase - Fa*cgh
+        eq3 = Fa - Ff*muTire
+
+        X = linsolve([eq1, eq2, eq3], (Fr, Ff, Fa))
+        #extract the solution for eq3
+        wheelForceMax = X.args[0][2]
+    else:
+
+        eq1 = Fr + Ff - mass*g
+        eq2 = Fr*(1-wtRearFrac)*wheelbase - Ff*wtRearFrac*wheelbase - Fa*cgh
+        eq3 = Fa - Fr*muTire
+
+        X = linsolve([eq1, eq2, eq3], (Fr, Ff, Fa))
+        #extract the solution for eq3
+        wheelForceMax = X.args[0][2]
+    
+    return wheelForceMax
+
+def accelerateVehicle(Cd, frontArea, mass, grade, v0, v1, cgh, wtRearFrac, wheelbase, driveWheel, \
+desiredAccTime, muTire, wheelRadius):
+    
+    #some constantas
+    rhoAir      = 1.2
+    g           = 9.81
+    Cr          = 0.01
+    transLoss   = 0.99**3
+    
+    #limit v1 max to
+    vmax = 151.0/3.6
+    if v1 > vmax:
+        v1 = vmax
+    
+    #initial value
+    acceleration = 0
+
+    #call wheelforcemax
+    wheelForceMax  = tractionMax(mass, g, wtRearFrac, wheelbase, cgh, driveWheel, muTire)
+    wheelTorqueMax = wheelForceMax * wheelRadius
+
+    #start search for optimal wheeltorque curve given constraints.
+    #initialize power and time with some high values
+    power       = 10e3
+    simAccTime  = 1e6
+
+    while abs(simAccTime - desiredAccTime) > 0.01:
+        #initialize temporary variables
+        iterStep = 1
+        timeStep = 0.01
+        vCur     = v0
+        pCar     = 0 #momentum?
+        timeTol  = 0.1
+
+        if simAccTime > desiredAccTime+timeTol:
+            power = power * (1+np.sqrt(5))/2
+        else:
+            power = power * 0.75
+
+        print('power=',power*1e-3) 
+        velocities  = []
+        torques     = []
+
+        while vCur < v1:
+            if vCur > 0:
+                wheelForce = min(wheelForceMax, power/vCur)
+            else:
+                wheelForce = wheelForceMax 
+
+            if wheelForce - topGradeSpeed(Cd, Cr, g, rhoAir, frontArea, mass, grade, vCur, wheelRadius)/wheelRadius <= 10:
+                break
+            else:
+                loadDiff = wheelForce - topGradeSpeed(Cd, Cr, g, rhoAir, frontArea, mass, grade, vCur, wheelRadius)/wheelRadius
+            fDiff = min(loadDiff*transLoss, wheelForceMax)
+            acceleration = fDiff / mass
+            vCur = vCur + acceleration*timeStep
+            velocities.append(vCur)
+            torques.append(wheelForce*wheelRadius)
+            pCar = pCar + vCur * timeStep
+
+            #all done, go to next timestep
+            iterStep += 1
+        #when a time is found, update the simulated acceleration time
+        simAccTime = iterStep*timeStep
+        
+        if power > 2e6:
+            print('Reqd power > 2 MW, its not gonna happen')
+            break
+
+    return torques, velocities, power
+
+
+if __name__ == "__main__":
+    mass = 1500
+    Cd = 0.3
+    frontArea = 2
+    grade = 0
+    v0 = 0
+    v1 = 100.0/3.6
+    cgh = 0.6
+    wtRearFrac = 0.56
+    wheelbase = 2.5
+    driveWheel = 'RWD'
+    desiredAccTime = 9
+    muTire = 0.9
+    wheelRadius = 0.3
+
+    torques, velocities, power = accelerateVehicle(Cd, frontArea, mass, grade, v0, v1, cgh, wtRearFrac, wheelbase, driveWheel, \
+    desiredAccTime, muTire, wheelRadius)
+
+    print(f'Power is {power*1e-3} kW')
