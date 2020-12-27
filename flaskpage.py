8a0a550026b61468d8045ffb8ca331655bd01e9f
Pontus <pontus@fyhr.nu>
2020-12-27 13:48:02 +0100

    css and templates

--- flaskpage.py
+++ flaskpage.py
@@ -1,14 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Index page'
+    return render_template('index.html')
 
-@app.route('/hello')
-def hello():
-    return 'Hello, how do you do world?'
 
 if __name__ == "__main__":
     app.run(debug=True)
