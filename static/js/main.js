e725fe1f1681b5f1068c74b5ea6be921e308ef19
Pontus <pontus@fyhr.nu>
2020-12-28 17:12:17 +0100

    finally, it works!!! horrible horrible javascript :)

--- static/js/main.js
+++ static/js/main.js
@@ -4,36 +4,66 @@
     })
 .then(jsondata => console.log(jsondata));*/
 
-async function getData() {
+/*async function getData() {
     let url = './static/json/torques126kW.json';
     try {
-        let res = await fetch(url);
-        return await res.json();
+        const res = await fetch(url);
+        const jsondata =  await res.json();
+        const textdata = await res.text();
+        console.log(jsondata)
+    } catch (error) {
+        console.log(error);
+    }
+}*/
+
+// Empty arrays for the data to be plotted
+const xvals = [];
+const yvals = [];
+
+async function getData() {
+    let url = './static/csv/torques126kW.csv';
+    try {
+        const res = await fetch(url);
+        const textdata = await (await res.text()).trim();
+        //console.log(textdata);
+        
+        const labels = textdata.split('\n').slice(0,1); //put labels here
+        const table = textdata.split('\n').slice(1); //keep datarows only
+        table.forEach(row => {
+            const columns = row.split(',')
+            const velocity = columns[0];
+            const torque = columns[1];
+            xvals.push(velocity);
+            yvals.push(torque);
+            console.log(velocity, torque);
+        });
     } catch (error) {
         console.log(error);
     }
 }
 
-let jsondata = getData()
-
-const ctx = document.getElementById('plot').getContext('2d');
-const myChart = new Chart(ctx, {
-    type: 'scatter',
-    data: {
-        datasets: [{
-            label: 'Scatter Dataset',
-            data: [{
-                x: jsondata.velocity,
-                y: jsondata.torque
-            }]
-        }]
-    },
-    options: {
-        scales: {
-            xAxes: [{
-                type: 'linear',
-                position: 'bottom'
-            }]
+async function makePlot() {
+    await getData();
+    const ctx = document.getElementById('plot').getContext('2d');
+    const myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: xvals,
+            datasets: [{
+                label: 'Generated torques',
+                data: yvals
+                }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            } 
         }
-    }
-});
+    });
+};
+
+makePlot();
