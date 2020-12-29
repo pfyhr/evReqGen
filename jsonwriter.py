39ea96d791012b914ba7fda4048c82563eaa62e4
Pontus <pontus@fyhr.nu>
2020-12-29 20:54:53 +0100

    nice json writer for the x-y data that chart.js seems to like

--- jsonwriter.py
+++ jsonwriter.py
@@ -1,8 +1,28 @@
+import numpy as np
+import json
+import pathlib
 
-def output_json(csvfile):
+def output_json(csvfile, name, power):
     #this will be the ugliest workaround ever.
-    data = np.genfromtxt(csvfile, skip_header=0)
+    data = np.genfromtxt(csvfile, skip_header=0, delimiter=',')
+    string = '{"name": [{'
+    for i,j in enumerate(data):
+        string += f'"x": {data[i][0]},'
+        string += f'"y": {data[i][1]}'
+        string += '},{'
+    string += '}],'
+    string += f'"{name}": {power}'
+    string += '}' 
+    print(string)
+
+    #make json file
+    fpath = pathlib.Path(csvfile)
+    print(fpath.stem)
+    string_dict = json.loads(string)
+    with open(f'./static/json/{fpath.stem}.json', 'w') as json_file:
+        json.dump(string_dict, json_file, indent = 4)
 
 
 if __name__ == "__main__":
-    output_json('static/csv/leafRealData.csv')
+    path = 'static/csv/leafRealData.csv'
+    output_json(path,'Leaf_real',120)
