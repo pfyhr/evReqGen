e853bbe28d1bb8b85b34e7786fe29974e45ece96
pf831 <pontus.fyhr@haldex.com>
2020-12-30 09:06:57 +0100

    nicer jsonwriter function, as suggested in https://github.com/pfyhr/dragraceSim/issues/1

--- jsonwriter.py
+++ jsonwriter.py
@@ -3,27 +3,22 @@ import json
 import pathlib
 
 def output_json(csvfile, name, power):
-    #this will be the ugliest workaround ever.
+    #load the csvfile
     data = np.genfromtxt(csvfile, skip_header=0, delimiter=',')
-    string = '{"name": [{' #hard to get the fstring for name in here...
-    for i,j in enumerate(data):
-        string += f'"x": {data[i][0]},'
-        string += f'"y": {data[i][1]}'
-        string += '},{'
-    string = string[:-2]
-    string += '],'
-    string += f'"{name}": {power}'
-    string += '}' 
-    print(string)
-
-    #make json file
+    
+    #make a dict of it, as naji suggested!
+    name = [{'x':i, 'y':j} for i,j in data] 
+    string_dict = {'name': name, 'Leaf_real': power}
+    
+    #find the filename and put ut in the desired path
     fpath = pathlib.Path(csvfile)
     print(fpath.stem)
-    string_dict = json.loads(string)
+    
+    #and write it to .json file
     with open(f'./static/json/{fpath.stem}.json', 'w') as json_file:
         json.dump(string_dict, json_file, indent = 4)
 
 
 if __name__ == "__main__":
-    path = 'static/csv/leafRealData.csv'
+    path = 'static/csv/egolfSimData.csv'
     output_json(path,'Leaf_real', 80)
