9e3b858e0fbe669d69f0287c83c66cb1f5f6f441
Pontus <pontus@fyhr.nu>
2021-01-03 14:13:50 +0100

    html input form, python function call!

--- flaskpage.py
+++ flaskpage.py
@@ -6,22 +6,34 @@ app = Flask(__name__)
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        arguments = request.form['content']
-        
+        Cd              = float(request.form['Cd'])
+        frontArea       = float(request.form['frontArea'])
+        mass            = float(request.form['mass'])
+        grade           = float(request.form['grade'])
+        v0              = float(request.form['v0'])
+        v1              = float(request.form['v1'])
+        cgh             = float(request.form['cgh'])
+        wtRearFrac      = float(request.form['wtRearFrac'])
+        wheelbase       = float(request.form['wheelbase'])
+        driveWheel      = request.form['driveWheel']
+        desiredAccTime  = float(request.form['desiredAccTime'])
+        muTire          = float(request.form['muTire'])
+        wheelRadius     = float(request.form['wheelRadius'])
+
         try:
             #a bit ugly oneliner to split input arguments, map them to floats, put them in a list
-            chunks = list(map(float, arguments.split(',')))
-            print(chunks)
-            if len(chunks) == 12:
-                torques, velocities, power = dragraceSim.accelerateVehicle(chunks[0], chunks[1], \
-                    chunks[2], chunks[3], chunks[4], chunks[5], chunks[6], chunks[7], chunks[8], 'RWD', \
-                        chunks[9], chunks[10], chunks[11])
-            else:
-                torques, velocities, power = dragraceSim.accelerateVehicle(0.35, 2, 1300, 0, 0, 33, 0.5, 0.57, 2.5, 'RWD', \
-                    6, 0.95, 0.3)
-                print(power/1e3)
-            message = dragraceSim.plot_png(velocities, torques)
-            print(message)
+            #chunks = list(map(float, arguments.split(',')))
+            #print(chunks)
+            #if len(chunks) == 12:
+            print('running with:', Cd, frontArea, mass, grade, v0, v1, cgh, wtRearFrac, \
+                 wheelbase, driveWheel, desiredAccTime, muTire, wheelRadius)
+
+            torques, velocities, power = dragraceSim.accelerateVehicle(Cd, frontArea, mass, \
+                 grade, v0, v1, cgh, wtRearFrac, wheelbase, driveWheel, desiredAccTime, muTire, wheelRadius)
+            
+            print(power/1e3)
+            #message = dragraceSim.plot_png(velocities, torques)
+            #print(message)
             return render_template('index.html')
         except:
             return 'some issue occured'
